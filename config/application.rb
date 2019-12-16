133f38c227ea2ce3870dadca13e0d10689c8e0f7
lethang7794 <lethang7794@gmail.com>
2019-12-16 09:30:14 +0700

    Filter out credit card number

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Depot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Filter out credit card number
+    config.filter_parameters += [ :credit_card_number ]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
