f35e049f6b6335db9e1682784464dcf06e3f1b8d
lethang7794 <lethang7794@gmail.com>
2019-12-16 11:15:01 +0700

    Update the test case for OrderMailer#received
    - Modify the fixture to match the test case.

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -2,18 +2,18 @@ require 'test_helper'
 
 class OrderMailerTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderMailer.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "Depot Store - Order Confirmation", mail.subject
+    assert_equal ["john@gmail.com"], mail.to
+    assert_equal ["customer@depot.com"], mail.from
+    assert_match "Dear John", mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderMailer.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Depot Store - Order Shipped", mail.subject
+    assert_equal ["john@gmail.com"], mail.to
+    assert_equal ["customer@depot.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
