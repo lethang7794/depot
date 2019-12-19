5aa75059386a4ac015425adf9f1aeaa74caa0660
lethang7794 <lethang7794@gmail.com>
2019-12-19 16:34:22 +0700

    Add placeholder to input field
    Of:
    - Order#_form
    - CreditCardPayType Component

--- app/javascript/PayTypeSelector/CreditCardPayType.jsx
+++ app/javascript/PayTypeSelector/CreditCardPayType.jsx
@@ -8,25 +8,29 @@ class CreditCardPayType extends React.Component {
           <label>Cardholder Name</label>
           <input  type="text"
                   name="order[cardholder_name]"
-                  id="order_cardholder_name"  />
+                  id="order_cardholder_name"
+                  placeholder="LE QUANG THANG"  />
         </div>
         <div>
           <label>Credit/Debit Card Number</label>
           <input  type="number"
                   name="order[card_number]"
-                  id="order_card_number"  />
+                  id="order_card_number"
+                  placeholder="1234 1234 1234 1234"  />
         </div>
         <div>
           <label>Expiration Date</label>
           <input  type="number"
                   name="order[expiration_date]"
-                  id="order_expiration_date"  />
+                  id="order_expiration_date"
+                  placeholder="01/19"  />
         </div>
         <div>
           <label>CVV</label>
           <input  type="number"
                   name="order[cvv]"
-                  id="order_cvv"  />
+                  id="order_cvv"
+                  placeholder="789"  />
         </div>
       </div>
     );
