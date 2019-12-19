aaf174ff357aff49dee6b823aa6d5bae95b6ef39
lethang7794 <lethang7794@gmail.com>
2019-12-19 11:34:52 +0700

    Change System Test configuration options
    (GoogleChrome can't start in WSL)
    - Set up a WebDriver Server run in Windows.
    - Config Selenium to run a remote WebDriver client with Chrome.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,15 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # Default
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  # Use a remote WebDriver sever
+  def setup
+    Capybara.register_driver "selenium_remote_chrome".to_sym do |app|
+      Capybara::Selenium::Driver.new(app, browser: :remote, url: "http://localhost:4445/wd/hub", desired_capabilities: :chrome)
+    end
+    Capybara.javascript_driver = :selenium_remote_chrome
+    Capybara.current_driver = :selenium_remote_chrome
+  end
 end
