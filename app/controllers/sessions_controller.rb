d27a53cb19c4a4a6c99a68ff79afe2cd34e563d5
lethang7794 <lethang7794@gmail.com>
2019-12-26 01:03:14 +0700

    Add feature to remember username in the login form
    - Because life is too short to reenter username if username/password is wrong.
    - Add autofocus to username field.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to admin_url
     else
+      session[:name] = params[:name]
       redirect_to login_url, alert: "Wrong password, please try again."
     end
   end
