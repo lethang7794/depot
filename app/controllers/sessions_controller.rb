dcd95612d376897f1dcc155395ca252b1557a55d
lethang7794 <lethang7794@gmail.com>
2019-12-25 20:07:13 +0700

    Add SessionsController#create for login feature
    - Redirect to admin page if username and password are correct.
    - Else, show login page to login again.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: params[:name])
+    if user.try(:authenticate, params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Wrong password, please try again."
+    end
   end
 
   def destroy
