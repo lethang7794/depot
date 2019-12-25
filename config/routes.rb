da6d5f84ca8dc3226e071e8807a7d995e906cebe
lethang7794 <lethang7794@gmail.com>
2019-12-25 19:58:14 +0700

    Add routes for login/logout feature
    Delete routes generated by rails generater command.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :users
   resources :orders
   resources :line_items
