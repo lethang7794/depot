200cf825c8e5d57c70322f91cc7e00e966c10a35
lethang7794 <lethang7794@gmail.com>
2019-12-26 15:42:48 +0700

    Add system test cases for user feature
    - Access index page
    - Create a user
    - Update a user
    - Destroy a user

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,7 +1,46 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "visiting the index" do
+    visit users_url
+    assert_selector "h1", text: "Users"
+  end
+
+  test "creating a User" do
+    User.delete_all
+    visit users_url
+    click_on "New User"
+
+    fill_in "user_name", with: @user.name
+    fill_in "user_password", with: 'secret'
+    fill_in "user_password_confirmation", with: 'secret'
+    click_on "Create User"
+
+    assert_text "User '#{@user.name}' was successfully created"
+  end
+
+  test "updating a User" do
+    visit users_url
+    click_on "Edit", match: :first
+
+    fill_in "user_name", with: @user.name
+    fill_in "user_password", with: 'secret'
+    fill_in "user_password_confirmation", with: 'secret'
+    click_on "Update User"
+
+    assert_text "User '#{@user.name}' was successfully updated"
+  end
+
+  test "destroying a User" do
+    visit users_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "User was successfully destroyed"
+  end
 end
