192f3d375b2323a2580d1fcdfb739231dad96674
lethang7794 <lethang7794@gmail.com>
2019-12-27 00:01:59 +0700

    Add basic limit access feature
    - Block access to all page.
    - Whitelist only login page for log in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
   # before_action :set_time
   #
   # def set_time
@@ -10,4 +11,11 @@ class ApplicationController < ActionController::Base
   def self.time
     @time
   end
+
+  protected
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to '/login', notice: "You need to log in to access this page."
+      end
+    end
 end
