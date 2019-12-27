78eb262594776b6133269833e2705b5158645516
lethang7794 <lethang7794@gmail.com>
2019-12-27 15:53:31 +0700

    Fix AdminControllerTest failure
    - Add setup

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    login_as users(:one)
+  end
+
   test "should get index" do
     get admin_url
     assert_response :success
