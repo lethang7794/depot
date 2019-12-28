e4d5f9ee9c91b137d7a5ec12d4104e1bb7839a3a
lethang7794 <lethang7794@gmail.com>
2019-12-28 15:39:30 +0700

    Fix bug in remember username in login form
    Change from remember username from last login failure attempt
    to last login attempt (both failure and success attempt).

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    session[:name] = params[:name]
     user = User.find_by(name: params[:name])
     if user.try(:authenticate, params[:password])
       session[:user_id] = user.id
       redirect_to admin_url
     else
-      session[:name] = params[:name]
       redirect_to login_url, alert: "Wrong password, please try again."
     end
   end
