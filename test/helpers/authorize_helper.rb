0b3c4cc504028cd809c69f4ab682ebefd80a59f0
lethang7794 <lethang7794@gmail.com>
2020-01-09 10:21:26 +0700

    Add login helper for system test

--- test/helpers/authorize_helper.rb
+++ test/helpers/authorize_helper.rb
@@ -6,4 +6,11 @@ module AuthorizeHelper
   def logout
     delete 'logout'
   end
+
+  def login_as_user(user)
+    visit '/login'
+    fill_in 'name', with: user.name
+    fill_in 'password', with: 'secret'
+    click_on 'commit'
+  end
 end
