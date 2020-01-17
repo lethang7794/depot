c327f0d1b6137dc57bc7e335d29433bd4c093ad1
lethang7794 <lethang7794@gmail.com>
2020-01-17 19:30:44 +0700

    Add validate for line item quantiy

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,6 +2,7 @@ class LineItem < ApplicationRecord
   belongs_to :order, optional: true
   belongs_to :product
   belongs_to :cart
+  validates :quantity, numericality: {greater_than: 0}
 
   def total_price
       product.try(:price) * quantity
